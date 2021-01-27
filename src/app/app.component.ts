e2c436ff7c164bf9b10bb404a2d66c59ae704ab5
Jamil Bailony <jBailony@outlook.com>
2021-01-27 22:25:43 +0300

    build the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wsearch';
+  onSearch(term: string) {
+    console.log(term);
+    
+  }
 }
--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent implements OnInit {
+  term = '';
+  @Output() submitted = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onFormSubmit(event: any) {
+    event.preventDefault();
+    this.submitted.emit(this.term);
+  }
 }
